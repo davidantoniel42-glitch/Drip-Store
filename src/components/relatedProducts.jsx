8c762c1ce1c45a17b091fd7e6419638af3b6d96e
David <davidantoniel42@gmail.com>
2026-03-06 22:25:57 -0300

    correção do productcardlist

--- src/components/relatedProducts.jsx
+++ src/components/relatedProducts.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import ProductCard from "./AbaProdutos/productcardlist";
+import ProductCard from "./AbaProdutos/Productcardlist";
 
 const RelatedProducts = ({ currentProduct, allProducts }) => {
   const related = allProducts
