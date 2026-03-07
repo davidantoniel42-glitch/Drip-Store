b356164c32f149ee7e71cae8921d9f74d598dcc1
David <davidantoniel42@gmail.com>
2026-03-06 21:09:03 -0300

    Correção  das configuração do vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"; // Importe o plugin
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
